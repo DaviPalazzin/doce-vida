61893f514a4e1cf1aeb4b8c6c424157ca8784d29
Davi Palazzin Pereira <115328246+DaviPalazzin@users.noreply.github.com>
2025-05-11 22:56:49 +0000

    correção do sql

--- assets/data/0-schema.sql
+++ assets/data/0-schema.sql
@@ -5,15 +5,14 @@ CREATE TABLE `usuarios` (
   `senha` varchar(100) NOT NULL,
   `reset_token` varchar(100) DEFAULT NULL,
   `reset_token_expira` datetime DEFAULT NULL,
+  `email_verificado` tinyint(1) DEFAULT '0',
+  `codigo_verificacao` varchar(6) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-LOCK TABLES `usuarios` WRITE;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 INSERT INTO `usuarios` VALUES 
-(1,'admin',NULL,'admin',NULL,NULL),
-(22,'teste','teste@ixeoxnph.mailosaur.net','$2y$10$MGHBj3NdoVaDz2U0QaxrZuAfiBN6.gs/hoYRegxaQ6Oy4PR0ODrpW',NULL,NULL),
-(23,'davi',NULL,'$2y$10$JjtLP5x8CV9/DTEyHphlWubdIKLa7ePR7d2/9H7ABgPro9xf7.t2a',NULL,NULL);
-
-UNLOCK TABLES;
+(1,'admin',NULL,'admin',NULL,NULL,0,NULL),
+(22,'teste','teste@ixeoxnph.mailosaur.net','$2y$10$MGHBj3NdoVaDz2U0QaxrZuAfiBN6.gs/hoYRegxaQ6Oy4PR0ODrpW',NULL,NULL,0,NULL),
+(23,'davi',NULL,'$2y$10$JjtLP5x8CV9/DTEyHphlWubdIKLa7ePR7d2/9H7ABgPro9xf7.t2a',NULL,NULL,0,NULL),
+(24,'Davi','davipalazzin14@gmail.com','$2y$10$mxymZEP2CByB59IRtEJPKuANg.crvNMp5A06UE9QGbiBwqR1taqz.',NULL,NULL,1,NULL);
